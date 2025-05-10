2f5ade6c37c96bfd7e555e71d22b2efb58b08891
madhurimadhuu <madhurimannem94@gmail.com>
2025-05-10 09:36:19 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,7 @@
+function showMessage() {
+  const messageElement = document.getElementById("message");
+  messageElement.textContent = "We will get back to you shortly!";
+  messageElement.style.color = "blue"; // Change to the desired color
+}
+
+
